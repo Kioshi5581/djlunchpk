b6dad0b9ce98900453f0970045e144f04996bc38
きよし 森 <98475952+Kioshimori@users.noreply.github.com>
2022-01-27 16:21:52 +0500

    Delete br.py

--- br.py
+++
@@ -1,7 +0,0 @@
-import requests
-import json
-
-response = requests.get("http://localhost:8000/apioverview/monthly-package/")
-print(response)
-json_data = response.json()['results']['package_name']
-print(json_data)
